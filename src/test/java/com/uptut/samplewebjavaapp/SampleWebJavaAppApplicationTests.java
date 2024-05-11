850927995b9a11a4e14aa717732e82e51bacc00d
Saurabh <saurabh.d2106@gmail.com>
2024-05-12 01:39:26 +0530

    Sample Java Application

---
+++ src/test/java/com/uptut/samplewebjavaapp/SampleWebJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptut.samplewebjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
